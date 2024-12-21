c1e2c6059d5d6524a0f2c2481206887b32544c68
Hamza <hamza_olgen@hotmail.com>
2024-12-22 00:28:23 +0300

    Patika dev Ortalama Hesaplama Ödevi

---
+++ src/Odevler/Odev03UcgenAlanHesaplama.java
@@ -0,0 +1,14 @@
+package Odevler;
+
+import java.util.Scanner;
+
+public class Odev03UcgenAlanHesaplama {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Üçgenin Dik Kenarını Giriniz: ");
+        double dikKenar= scan.nextInt();
+        System.out.print("Taban Kenarı Giriniz: ");
+        double tabanKenar=scan.nextInt();
+        System.out.println("Üçgenin Alanı: " + (dikKenar*tabanKenar/2));
+    }
+}
