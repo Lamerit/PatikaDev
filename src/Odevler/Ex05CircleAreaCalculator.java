63b65c550261d54636b936b0b370ed8882174bf8
Hamza <hamza_olgen@hotmail.com>
2024-12-22 09:45:34 +0300

    Patika dev Ortalama Hesaplama Ödevi

---
+++ src/Odevler/Ex05CircleAreaCalculator.java
@@ -0,0 +1,17 @@
+package Odevler;
+
+import java.util.Scanner;
+
+public class Ex05CircleAreaCalculator {
+    public static void main(String[] args) {
+        //r is Radius
+        //Circle Area Formula is : π * r * r
+        //Circle Perimeter Formula is : 2 * π * r
+        Scanner scan= new Scanner(System.in);
+        System.out.print("Please input radius : ");
+        double r= scan.nextDouble();
+        double area=3.14*(r*r);
+        double perimeter=2*(3.14*r);
+        System.out.printf("Area Of The Circle = %.2f\nPerimeter Of The Circle = %.2f", area,perimeter);
+    }
+}
