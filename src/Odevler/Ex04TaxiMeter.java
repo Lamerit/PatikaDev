7b5c0489570d7b2442bbb96b48c8f49f48c3e87a
Hamza <hamza_olgen@hotmail.com>
2024-12-22 09:28:14 +0300

    Patika dev Ortalama Hesaplama Ödevi

---
+++ src/Odevler/Ex04TaxiMeter.java
@@ -0,0 +1,19 @@
+package Odevler;
+
+import java.util.Scanner;
+
+public class Ex04TaxiMeter {
+    public static void main(String[] args) {
+        //Acilis ucreti 10tl, En dusuk odenecek tutar 20tl, Kmbasına 2.2tl
+        Scanner scan = new Scanner(System.in);
+        double perKM = 2.20, openingAmount = 10, total;
+        System.out.print("======================================" +
+                "\nKatedilen Mesafeyi Km Cinsinden giriniz: ");
+        double km = scan.nextDouble();
+        total = (perKM * km) + openingAmount;
+        total = (total < 20) ? 20 : total;
+        System.out.format("Ödenecek tutar = %.2f\n======================================", total);
+
+    }
+
+}
