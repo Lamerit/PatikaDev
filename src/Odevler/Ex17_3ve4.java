aadc06e264b26d09e02bfc86abe96a193a977ed4
Hamza <hamza_olgen@hotmail.com>
2024-12-27 20:21:47 +0300

    Patika dev Ortalama Hesaplama Ödevi

--- src/Odevler/Ex17_3ve4.java
+++ src/Odevler/Ex17_3ve4.java
@@ -2,7 +2,7 @@ package Odevler;
 
 import java.util.Scanner;
 
-public class Ex173ve4 {
+public class Ex17_3ve4 {
     //0'dan girilen sayıya kadar olan sayılardan 3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
 
     public static void main(String[] args) {
