368e8de36f38f6cb91dcf577a7c294e6e3e79503
Hamza <hamza_olgen@hotmail.com>
2024-12-27 22:26:45 +0300

    Patika dev Ortalama Hesaplama Ödevi

---
+++ src/Odevler/Ex21UsluSayilar.java
@@ -0,0 +1,20 @@
+package Odevler;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Ex21UsluSayilar {
+    public static void main(String[] args) {
+        //Kullanıcıdan 2 değer al ve hesaplat
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Sayı giriniz: ");
+        int sayi=scan.nextInt();
+        System.out.print("Üs giriniz: ");
+        int us= scan.nextInt();
+        int toplam=1;
+        for (int i = 1; i <=us ; i++) {
+            toplam=sayi*toplam;
+        }System.out.println(toplam);
+        scan.close();
+    }
+}
